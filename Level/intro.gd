9c83e3925d2e53a3ea7b8bdaa84589229135d332
Cauê Melo <cauesmelo@gmail.com>
2022-11-20 22:14:58 -0300

    feat: add intro dialog

---
+++ Level/intro.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+@onready var text_node = $Text
+@onready var proceed_node = $Proceed
+
+var part = 0
+var acc = 0
+var is_done = false
+
+var labels = [
+"Luana é uma estudante de ciências da natureza na Unirio, mas apesar de já estar no terceiro período, não tem certeza se esse curso é o curso ideal para ela.",
+"Após refletir brevemente em seu quarto, se questiona se é possível mudar de curso sem precisar solicitar matrícula pelo SISU novamente.",
+"Sua missão é ajudar a Luana a realizar esse processo. Boa sorte!"
+]
+
+func print_text():
+	var current_index = len(text_node.text)
+		
+	if(current_index < len(labels[part])):
+		text_node.text = labels[part].substr(0, current_index + 1)
+	else:
+		proceed_node.visible = true
+		is_done = true
+
+func _process(delta):
+	acc = acc + delta
+	
+	if(acc > 0.8):
+		print_text()
+
+func _input(event):
+	if(Input.get_action_strength("action") == 1 && is_done):
+		print(part)
+		if(part < 2):
+			part = part + 1
+			text_node.text = ""
+			proceed_node.visible = false
+		else:
+			get_tree().change_scene_to_file("res://Level/luana_room.tscn")
