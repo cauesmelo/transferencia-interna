cf7ce3f0db294cc2e4752c9ddf853232922e9058
Cauê Melo <cauesmelo@gmail.com>
2022-12-02 07:58:21 -0300

    add all scenes

--- Script/escada_sala.gd
+++ Script/escada_sala.gd
@@ -1,5 +1,9 @@
 extends Area2D
 
+@onready var globals = get_node("/root/Globals")
+
 func _on_area_2d_body_entered(body):
 	if(body.name == "Luana"):
-		get_tree().change_scene_to_file("res://Level/luana_room.tscn")
+		globals.pos = 1
+		# get_tree().change_scene_to_file("res://Level/luana_room.tscn")
+		get_tree().change_scene_to_packed(globals.scene_room)
