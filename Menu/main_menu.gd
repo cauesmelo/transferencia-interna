1de18a04de65e6667611ee1a4414300a0fd87360
Cauê Melo <cauesmelo@gmail.com>
2022-12-02 13:39:11 -0300

    add song

--- Menu/main_menu.gd
+++ Menu/main_menu.gd
@@ -1,5 +1,10 @@
 extends Control
 
-func _process(_delta):
-	if(Input.get_action_strength("action") == 1):
+var acc = 0
+
+func _process(delta):
+	acc += delta
+
+func _input(_event):
+	if(Input.get_action_strength("action") == 1 && acc > 1):
 		get_tree().change_scene_to_file("res://Level/intro.tscn")
